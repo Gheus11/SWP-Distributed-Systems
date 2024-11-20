9d0656739ee56639a9ec3f0cf33fb7a26cabbacc
jaspee95 <jasperebbelaar@gmail.com>
2024-11-20 12:39:50 +0100

    home page with links and made login required

--- mySWP/myapp/urls.py
+++ mySWP/myapp/urls.py
@@ -1,12 +1,16 @@
 # myapp/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.redirect_to_login, name='redirect_to_login'),
     path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
     path('create_node/', views.create_node, name='create_node'),
     path('stop_node/', views.stop_node, name='stop_node'),
     path('connect_node/', views.connect_node, name='connect_node'),
     path('disconnect_node/', views.disconnect_node, name='disconnect_node'),
+    path('unauthorized/', views.unauthorized_response, name='unauthorized'),
+    path('home/', views.home, name='home'),
 ]
--- mySWP/myapp/views.py
+++ mySWP/myapp/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Welcome to the Hornet node API!")
+def redirect_to_login(request):
+    return redirect("/login/")
 
 def login_view(request):
     if request.method == "POST":
@@ -14,21 +15,37 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponse("Logged in successfully!")
+            return redirect("/home/")
         else:
             return render(request, 'login.html', {'error_message': "Invalid credentials."})
     return render(request, 'login.html')  # Render a login form
 
+def unauthorized_response(request):
+    return HttpResponse("You must be logged in to access this page.", status=403)
+
+
+@login_required(login_url="/unauthorized/")
 def create_node(request):
     return HttpResponse("Create node page.")
 
+@login_required(login_url="/unauthorized/")
 def stop_node(request):
     return HttpResponse("Stop node page.")
 
+@login_required(login_url="/unautorized/")
 def connect_node(request):
     return HttpResponse("Connect node page.")
 
+@login_required(login_url="/unauthorized/")
 def disconnect_node(request):
     return HttpResponse("Disconnect node page.")
+
+
+
+@login_required(login_url="/unauthorized")
+def home(request):
+    return render(request, 'home.html')  # Render the home page
+
+
 
 
