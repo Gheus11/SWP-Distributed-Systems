1a15d7a3b29ff59951b1d394a205b9bbdaf811ac
jaspee95 <jasperebbelaar@gmail.com>
2025-01-30 13:33:32 +0100

    updated connect and stop

--- DjangoServer/myapp/webapi.py
+++ DjangoServer/myapp/webapi.py
@@ -36,58 +36,76 @@ def get_id(container_name):
 
 
 def create_node_web(request, create_number):
-    # Change the working directory of this .py file: You must execute "docker 
-    # compose" command in the dir where the docker-compose.yml exists. Otherwise, 
-    # the "docker compose" command cannot identify the configuration file 
-    # "docker-compose.yml". This command tend to find the configuration file in the 
-    # dir where the command is executed.
-    current_dir = os.getcwd()
-    node_dir = f"./nodes/node_{create_number}"
-    os.chdir(node_dir)
-    print(f"before: {current_dir}")
-    print(f"after: {os.getcwd()}")
+    if not create_number.isdigit(): # if it's not a number
+        print("Error: Node number must be a valid number.")
+    elif get_container_by_name(f"hornet-{create_number.zfill(2)}"):  # if it already exists
+        print(f"Error: hornet-{create_number} already exists")
+    else:
+        if int(create_number) < 0 or int(create_number) > 99: # if it's not between 0 ~ 99
+            print("Error: Node number must be between 0 and 99.")
+        else:
+            create_number = create_number.zfill(2)
+            # Change the working directory of this .py file: You must execute "docker 
+            # compose" command in the dir where the docker-compose.yml exists. Otherwise, 
+            # the "docker compose" command cannot identify the configuration file 
+            # "docker-compose.yml". This command tend to find the configuration file in the 
+            # dir where the command is executed.
+            current_dir = os.getcwd()
+            node_dir = f"./nodes/node_{create_number}"
+            os.chdir(node_dir)
+            print(f"before: {current_dir}")            
+            print(f"after: {os.getcwd()}")
 
-    # Script paths
-    bootstrap_path = "./bootstrap.sh"
-    run_path = "./run.sh"
+            # Script paths
+            bootstrap_path = "./bootstrap.sh"
+            run_path = "./run.sh"
+            cleanup_path = "./cleanup.sh"
 
-    try:
-        # Add execute permission only to the owner (user)
-        subprocess.run(["chmod", "u+x", bootstrap_path], check=True)
-        subprocess.run(["chmod", "u+x", run_path], check=True)
-        # Execute bootstrap.sh & run.sh
-        subprocess.run([bootstrap_path, "build"], check=True)
-        subprocess.run([run_path, "-d"], check=True)
-        print(f"Hornet-{create_number} created.")
-    except subprocess.CalledProcessError as e:
-        messages.error(request,f"Error occured while running hornet-{create_number} : {e}")
-    finally:
-        # Recover the working dir path to its original state
-        os.chdir(current_dir)
-
+            try:
+                
+                # Add execute permission only to the owner (user)
+                subprocess.run(["chmod", "u+x", bootstrap_path], check=True)
+                subprocess.run(["chmod", "u+x", run_path], check=True)
+                subprocess.run(["chmod", "u+x", cleanup_path], check=True)
+                # Execute bootstrap.sh & run.sh
+                subprocess.run(["sudo", bootstrap_path], check=True)
+                subprocess.run(["sudo", run_path, "-d"], check=True)
+                print(f"Hornet-{create_number} created.")
+            except subprocess.CalledProcessError as e:
+                messages.error(request,f"Error occured while running hornet-{create_number} : {e}")
+            finally:
+                # Recover the working dir path to its original state
+                os.chdir(current_dir)
 
 def stop_node(request, stop_number):
-    current_dir = os.getcwd()
-    node_dir = f"./nodes/node_{stop_number}"
-    os.chdir(node_dir)
-    print(f"before: {current_dir}")
-    print(f"after: {os.getcwd()}")
+    if not stop_number.isdigit():   # if it's not a number
+        print("Error: Node number must be a valid number.")
+    elif get_container_by_name(f"hornet-{stop_number.zfill(2)}") is None:    # if it does not exist
+        print(f"Error: hornet-{stop_number} does not exist.")
+    else:
+        if int(stop_number) < 0 or int(stop_number) > 99: # if it's not between 0 ~ 99
+            print("Error: Node number must be between 0 and 99.")
+        else:
+            stop_number = stop_number.zfill(2)
+            current_dir = os.getcwd()
+            node_dir = f"./nodes/node_{stop_number}"
+            os.chdir(node_dir)
+            print(f"before: {current_dir}")
+            print(f"after: {os.getcwd()}")
 
-    # Script paths
-    cleanup_path = "./cleanup.sh"
+            # Script paths
+            cleanup_path = "./cleanup.sh"
 
-    try:
-        # Add execute permission only to the owner (user)
-        subprocess.run(["chmod", "u+x", cleanup_path], check=True)
-        # Execute
-        subprocess.run(["docker", "compose", "--profile", "4-nodes", "down"], check=True)
-        subprocess.run([cleanup_path], check=True)
-        print(f"Hornet-{stop_number} stopped.")
-    except subprocess.CalledProcessError as e:
-        messages.error(request, f"Error occurred while stopping hornet-{stop_number}: {e}")
-    finally:
-        # Recover the working dir path to its original state
-        os.chdir(current_dir)
+            try:
+                # Execute
+                subprocess.run(["sudo", "docker", "compose", "--profile", "4-nodes", "down"], check=True)
+                subprocess.run(["sudo", cleanup_path], check=True)
+                print(f"Hornet-{stop_number} stopped.")
+            except subprocess.CalledProcessError as e:
+                messages.error(request, f"Error occurred while stopping hornet-{stop_number}: {e}")
+            finally:
+                # Recover the working dir path to its original state
+                os.chdir(current_dir)
 
 
 client_ = docker.from_env()
